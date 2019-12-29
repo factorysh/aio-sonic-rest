62c06428ce913541eda14d8b9a0f697c74730387
Mathieu Lecarme <mlecarme@bearstech.com>
2019-12-29 12:22:53 +0100

    Less data

--- collection_test.py
+++ collection_test.py
@@ -3,10 +3,10 @@ from collection import CollectionSerializer, CollectionReader
 
 
 def test_Collection():
-    with CollectionSerializer("data_test") as c:
+    with CollectionSerializer("data/test") as c:
         c.mkdir()
         c.append(dict(name="Pépé", age=42))
         c.append(dict(name="Bob", age=12))
 
-    with CollectionReader("data_test") as r:
+    with CollectionReader("data/test") as r:
         assert json.loads(r[1])["name"] == "Bob"
