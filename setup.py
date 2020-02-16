1a81b96633a41406db202026e7143785badb4a7f
Mathieu Lecarme <mlecarme@bearstech.com>
2020-02-16 23:47:32 +0100

    Black.

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='sonic_rest',
-    author='Mathieu Lecarme',
-    version='0.1.0',
-    packages=['sonic_rest'],
-    keywords=['sonic', 'search', 'asyncio'],
-    url='https://github.com/facttorysh/aio-simple-search',
-    license='3 terms BSD licence',
-    long_description='',
-    description='REST for sonic',
-    install_requires=[
-        'sonic-client',
-        'asonic',
-        'aiohttp[speedups]',
-    ],
-    extras_require={
-        'tests': ['pytest', 'pytest-aiohttp'],
-    },
+    name="sonic_rest",
+    author="Mathieu Lecarme",
+    version="0.1.0",
+    packages=["sonic_rest"],
+    keywords=["sonic", "search", "asyncio"],
+    url="https://github.com/facttorysh/aio-simple-search",
+    license="3 terms BSD licence",
+    long_description="",
+    description="REST for sonic",
+    install_requires=["sonic-client", "asonic", "aiohttp[speedups]",],
+    extras_require={"tests": ["pytest", "pytest-aiohttp"],},
 )
