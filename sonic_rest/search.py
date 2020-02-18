be6f3e5612c483bb457ca4386389766365e36b5c
Mathieu Lecarme <mlecarme@bearstech.com>
2020-02-18 10:47:38 +0100

    Suggest.

--- sonic_rest/search.py
+++ sonic_rest/search.py
@@ -34,3 +34,18 @@ class Search:
     async def search(self, query, fields):
         size, results = await self.search_naked(query, fields)
         return size, (json.loads(r) for r in results)
+
+    async def suggest(self, query, fields):
+        await self.client.channel(Channels.SEARCH.value)
+        futures = dict()
+        for field in fields:
+            futures[field] = ensure_future(
+                self.client.suggest(self.site, field, query, 5)
+            )
+        await gather(*futures.values())
+        already = set()
+        suggestions = list()
+        for field in fields:
+            suggestions += [a for a in futures[field].result() if a not in already]
+            already = already.union(suggestions)
+        return suggestions
