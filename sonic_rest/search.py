cc4a711ac7c1398beccb9cba152a88d06bfb09a8
Gael Pasgrimaud <gael@gawel.org>
2020-02-19 11:15:42 +0100

    handle no query; do not set channel twice

--- sonic_rest/search.py
+++ sonic_rest/search.py
@@ -8,23 +8,35 @@ from .collection import CollectionReader
 
 
 class Search:
-    def __init__(self, store, host="127.0.0.1", port=1491, password=None, site="site"):
+    def __init__(self, store,
+                 host="127.0.0.1", port=1491, password=None,
+                 site="site"):
         self.client = Client(
             host=host, port=port, password=password, max_connections=100
         )
         self.site = site
         self.collection = CollectionReader(store)
+        self.channel = None
+
+    async def set_search_channel(self):
+        if self.channel is None:
+            await self.client.channel(Channel.SEARCH)
+            self.channel = True
 
     async def search_naked(self, query, fields):
-        await self.client.channel(Channel.SEARCH)
+        await self.set_search_channel()
         futures = dict()
         for field in fields:
-            futures[field] = ensure_future(self.client.query(self.site, field, query))
+            futures[field] = ensure_future(
+                self.client.query(self.site, field, query))
         await gather(*futures.values())
         already = set()
         results = list()
         for field in fields:
-            ids = [int(id) for id in futures[field].result() if int(id) not in already]
+            ids = [
+                int(id) for id in futures[field].result()
+                if int(id) not in already
+            ]
             if len(ids) == 0:
                 continue
             already = already.union(ids)
@@ -36,7 +48,7 @@ class Search:
         return size, (json.loads(r) for r in results)
 
     async def suggest(self, query, fields):
-        await self.client.channel(Channel.SEARCH)
+        await self.set_search_channel()
         futures = dict()
         for field in fields:
             futures[field] = ensure_future(
@@ -46,6 +58,9 @@ class Search:
         already = set()
         suggestions = list()
         for field in fields:
-            suggestions += [a for a in futures[field].result() if a not in already]
+            suggestions += [
+                a for a in futures[field].result()
+                if a not in already
+            ]
             already = already.union(suggestions)
         return suggestions
--- sonic_rest/web.py
+++ sonic_rest/web.py
@@ -5,26 +5,26 @@ Sonic stores nothing, search returns just a collection of id.
 Query return stored documents, already serialized.
 """
 from aiohttp import web
-from asonic import Client
-
-from .collection import CollectionReader
-from .search import Search
 
 
 async def query(request):
     resp = web.StreamResponse(
         status=200, reason="OK", headers={"Content-Type": "application/json"}
     )
+    query = request.query.get('q')
     await resp.prepare(request)
-    await resp.write(b"[")
-    size, results = await request.app["search"].search_naked(
-        request.query["q"], request.app["fields"]
-    )
-    for i, r in enumerate(results):
-        await resp.write(r)
-        if i + 1 < size:
-            await resp.write(b",")
-    await resp.write(b"]")
+    if not query:
+        await resp.write(b'{"error": "no query provided"}')
+    else:
+        await resp.write(b'{"results": [')
+        size, results = await request.app["search"].search_naked(
+            query, request.app["fields"]
+        )
+        for i, r in enumerate(results):
+            await resp.write(r)
+            if i + 1 < size:
+                await resp.write(b",")
+        await resp.write(b"]}")
     await resp.drain()
     return resp
 
@@ -37,9 +37,9 @@ async def suggest(request):
     return web.json_response([j.decode("utf8") for j in suggestions])
 
 
-def sonic_rest(app, search, root="/"):
+def sonic_rest(app, search, fields=['tags', 'body'], root="/"):
     app["search"] = search
-    app["fields"] = ["tags", "body"]
+    app["fields"] = fields
     app.add_routes([
         web.get("%squery" % root, query),
         web.get("%ssuggest" % root, suggest),
